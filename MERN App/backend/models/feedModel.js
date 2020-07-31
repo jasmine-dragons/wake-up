36542a1c27d6a33fd30be6f3de6f22c747d04db8
PrateekS1917 <61072843+PrateekS1917@users.noreply.github.com>
2020-07-30 22:30:04 -0700

    Fully working CRUD backend

---
+++ MERN App/backend/models/feedModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const feedSchema = new Schema({
+  title: { type: String, required: true },
+  location: { type: String, required: true },
+  theme: { type: String, required: true },
+});
+feed = mongoose.model("feed", feedSchema);
+module.exports = feed;
---
+++ MERN App/backend/routes/feedRoute.js
@@ -0,0 +1,54 @@
+const Router = require("express").Router();
+let feedItem = require("../models/feedModel");
+
+Router.route("/get").get((req, res) => {
+  feedItem
+    .find()
+    .then((feed) => res.json(feed))
+    .catch((err) => res.status(400).json("Error" + err));
+});
+
+Router.route("/").post((req, res) => {
+  const title = req.body.title;
+  const location = req.body.location;
+  const theme = req.body.theme;
+
+  const newItem = new feedItem({ title, location, theme });
+
+  newItem
+    .save()
+    .then(() => res.json("item added"))
+    .catch((err) => res.status(400).json("error" + err));
+});
+
+Router.route("/:id").get((req, res) => {
+  feedItem
+    .findById(req.params.id)
+    .then((todo) => res.json(todo))
+    .catch((err) => res.status(400).json("error" + err));
+});
+
+Router.route("/:id").delete((req, res) => {
+  feedItem
+    .findByIdAndDelete(req.params.id)
+    .then(() => res.json("deleted"))
+    .catch((err) => res.status(400).json("error" + err));
+});
+
+Router.route("/update/:id").post((req, res) => {
+  feedItem
+    .findById(req.params.id)
+    .then((item) => {
+      item.title = req.body.title;
+      item.location = req.body.location;
+      item.theme = req.body.theme;
+
+      item
+        .save()
+        .then((item) => res.json("updated"))
+        .catch((err) => res.status(400).json("error" + err));
+    })
+    .catch((err) => res.status(400).json("error" + err));
+});
+
+module.exports = Router;
