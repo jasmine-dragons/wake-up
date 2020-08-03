06b1028d1a75fd4ee1cddd312a962947771689ca
PrateekS1917 <61072843+PrateekS1917@users.noreply.github.com>
2020-08-02 21:46:53 -0700

    Search Feature

--- MERN App/src/components/Search.component.js
+++ MERN App/src/components/Search.component.js
@@ -29,61 +29,85 @@ export default class Search extends Component {
     });
   }
 
-  searchResults() {
-    /*
-    var searchList = [];
-    if (this.state.action) {
-      searchList = this.state.searchRes.map((res) => {
-        if (res.action) {
-
-          return <Article info={res} key={res._id} />;
-        }
-      });
-    } else {
-      searchList = this.state.searchRes.map((res) => {
-        if (!res.action) {
-          return <Article info={res} key={res._id} />;
-        }
-      });
-    }
-    //console.log(this.state.action); */
-    //return searchList;
-  }
   onSubmit() {
     axios.get("http://localhost:5000/feed/get").then((res) => {
       this.setState({
         searchRes: res,
       });
     });
-
-    console.log(this.state.searchRes);
+    this.state.searchRes.map((res) => {
+      res.action
+        ? this.setState({
+            actionList: res,
+          })
+        : this.setState({
+            nonActionList: res,
+          });
+      return 0;
+    });
+  }
+  actionDisplay() {
+    const actlist = this.state.actionList.map((cur) => {
+      if (this.state.search === cur.title) {
+        return (
+          <div>
+            <br />
+            <Article info={cur} key={cur._id} />
+            <br />
+          </div>
+        );
+      } else return 0;
+    });
+    actlist.reverse();
+    return actlist;
+  }
+  nonActionDisplay() {
+    const nonactlist = this.state.nonActionList.map((cur) => {
+      if (this.state.search === cur.title) {
+        return (
+          <div>
+            <br />
+            <Article info={cur} key={cur._id} />
+            <br />
+          </div>
+        );
+      } else return 0;
+    });
+    nonactlist.reverse();
+    return nonactlist;
   }
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
-        <Form.Group
-          controlId="searchBar"
-          style={{ width: 400 }}
-          className="mx-auto"
-        >
-          <Form.Control
-            name="search"
-            value={this.state.search}
-            onChange={this.handleChange}
-            placeholder=" Learn a Little More"
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check
-            type="checkbox"
-            onChange={this.actionChange}
-            label="Find Places to Take Action"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" style={{ width: 300 }}>
-          Search!
-        </Button>
-      </Form>
+      <div>
+        {" "}
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group
+            controlId="searchBar"
+            style={{ width: 400 }}
+            className="mx-auto"
+          >
+            <Form.Control
+              name="search"
+              value={this.state.search}
+              onChange={this.handleChange}
+              placeholder=" Learn a Little More"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check
+              type="checkbox"
+              onChange={this.actionChange}
+              label="Find Places to Take Action"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" style={{ width: 300 }}>
+            Search!
+          </Button>
+        </Form>
+        <div>
+          {this.state.action ? this.actionDisplay() : this.nonActionDisplay()}
+        </div>
+      </div>
     );
   }
 }
